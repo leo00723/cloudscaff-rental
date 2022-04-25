2bb117137066d983b5888ca08f57d1371b51a1b9
leonardo <pillayleonardo@gmail.com>
2022-04-25 16:03:04 +0200

    added credit note and modification customer pages

--- src/app/customer-credit/customer-credit.module.ts
+++ src/app/customer-credit/customer-credit.module.ts
@@ -1,20 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
-import { IonicModule } from '@ionic/angular';
-
+import { ComponentsModule } from '../components/components.module';
 import { CustomerCreditPageRoutingModule } from './customer-credit-routing.module';
-
 import { CustomerCreditPage } from './customer-credit.page';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    CustomerCreditPageRoutingModule
-  ],
-  declarations: [CustomerCreditPage]
+  imports: [ComponentsModule, CustomerCreditPageRoutingModule],
+  declarations: [CustomerCreditPage],
 })
 export class CustomerCreditPageModule {}
--- src/app/customer-modification/customer-modification.module.ts
+++ src/app/customer-modification/customer-modification.module.ts
@@ -1,20 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
-import { IonicModule } from '@ionic/angular';
-
+import { ComponentsModule } from '../components/components.module';
 import { CustomerModificationPageRoutingModule } from './customer-modification-routing.module';
-
 import { CustomerModificationPage } from './customer-modification.page';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    CustomerModificationPageRoutingModule
-  ],
-  declarations: [CustomerModificationPage]
+  imports: [ComponentsModule, CustomerModificationPageRoutingModule],
+  declarations: [CustomerModificationPage],
 })
 export class CustomerModificationPageModule {}
