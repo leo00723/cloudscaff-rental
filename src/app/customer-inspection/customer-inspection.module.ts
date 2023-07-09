e5b74085749af9de050f4f2b84e5981c262b9064
Leo <pillayleonardo@gmail.com>
2023-07-09 19:54:34 +0200

    added uploads to customer inspection, handover

--- src/app/customer-inspection/customer-inspection.module.ts
+++ src/app/customer-inspection/customer-inspection.module.ts
@@ -1,20 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
-import { IonicModule } from '@ionic/angular';
 
 import { CustomerInspectionPageRoutingModule } from './customer-inspection-routing.module';
 
+import { ComponentsModule } from '../components/components.module';
 import { CustomerInspectionPage } from './customer-inspection.page';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    CustomerInspectionPageRoutingModule,
-  ],
+  imports: [ComponentsModule, CustomerInspectionPageRoutingModule],
   declarations: [CustomerInspectionPage],
 })
 export class CustomerInspectionPageModule {}
