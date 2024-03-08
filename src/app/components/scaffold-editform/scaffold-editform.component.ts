6ac8a7bcf0b5aba908f62cfaf17a9a938d98e132
Leo <pillayleonardo@gmail.com>
2024-03-08 09:51:05 +0200

    updated inspection

--- src/app/components/scaffold-editform/scaffold-editform.component.ts
+++ src/app/components/scaffold-editform/scaffold-editform.component.ts
@@ -2,6 +2,7 @@ import {
   ChangeDetectionStrategy,
   Component,
   EventEmitter,
+  Input,
   OnDestroy,
   OnInit,
   Output,
@@ -17,6 +18,7 @@ import { Select } from '@ngxs/store';
 import { Observable, Subscription } from 'rxjs';
 import { Company } from 'src/app/models/company.model';
 import { Item } from 'src/app/models/item.model';
+import { Scaffold } from 'src/app/models/scaffold.model';
 import { NotificationService } from 'src/app/services/notification.service';
 
 @Component({
@@ -26,6 +28,7 @@ import { NotificationService } from 'src/app/services/notification.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ScaffoldEditformComponent implements OnInit, OnDestroy {
+  @Input() scaffold: Scaffold;
   @Select() company$: Observable<Company>;
   form: FormGroup;
   @Output() formResult = new EventEmitter<{ boards: Item[]; scaffold: Item }>();
@@ -39,16 +42,45 @@ export class ScaffoldEditformComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
+    const handover = this.scaffold?.latestHandover?.scaffold || null;
     this.form = this.fb.group({
       scaffold: this.fb.group({
-        length: ['', [Validators.required, Validators.min(0)]],
-        width: ['', [Validators.required, Validators.min(0)]],
-        height: ['', [Validators.required, Validators.min(0)]],
-        safe: ['', [Validators.required]],
+        length: [
+          handover?.scaffold?.length || '',
+          [Validators.required, Validators.min(0)],
+        ],
+        width: [
+          handover?.scaffold?.width || '',
+          [Validators.required, Validators.min(0)],
+        ],
+        height: [
+          handover?.scaffold?.height || '',
+          [Validators.required, Validators.min(0)],
+        ],
+        safe: [handover?.scaffold?.safe || '', [Validators.required]],
         level: [1, [Validators.nullValidator]],
       }),
       attachments: this.fb.array([]),
       boards: this.fb.array([]),
+    });
+    handover?.attachments.forEach((item) => {
+      const attachment = this.fb.group({
+        length: [item.length || '', [Validators.required, Validators.min(0)]],
+        width: [item.width || '', [Validators.required, Validators.min(0)]],
+        height: [item.height || '', [Validators.required, Validators.min(0)]],
+        safe: [item.safe || '', [Validators.required]],
+        level: [item.level || '', [Validators.nullValidator]],
+      });
+      this.attachmentForms.push(attachment);
+    });
+    handover?.boards.forEach((item) => {
+      const board = this.fb.group({
+        length: [item.length || '', [Validators.required, Validators.min(0)]],
+        width: [item.width || '', [Validators.required, Validators.min(0)]],
+        height: [item.height || '', [Validators.required, Validators.min(0)]],
+        qty: [item.qty || '', [Validators.required, Validators.min(1)]],
+      });
+      this.boardForms.push(board);
     });
     this.subs.add(
       this.form.valueChanges.subscribe((value) => {
