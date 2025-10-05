8647e53cb5bb314ddd4583d4e60082c8e241eb93
Leo Pillay <pillayleonardo@gmail.com>
2025-10-05 16:54:17 +0200

    fixed date picker

--- src/app/components/datepicker/datepicker.component.ts
+++ src/app/components/datepicker/datepicker.component.ts
@@ -5,7 +5,7 @@ import {
   ViewChild,
 } from '@angular/core';
 import { IonDatetime, ModalController } from '@ionic/angular';
-import { format, parseISO } from 'date-fns';
+import { format, parseISO, parse } from 'date-fns';
 
 @Component({
   selector: 'app-datepicker',
@@ -17,7 +17,19 @@ export class DatepickerComponent {
   @Input() value: string | undefined;
   @Input() min: string | undefined;
   @Input() field: string;
+
   constructor(private modalController: ModalController) {}
+
+  get isoValue(): string | undefined {
+    if (!this.value) return undefined;
+    try {
+      const parsedDate = parse(this.value, 'dd-MM-yyyy', new Date());
+      return format(parsedDate, 'yyyy-MM-dd');
+    } catch {
+      return undefined;
+    }
+  }
+
   confirm() {
     this.date.confirm(false).then(() => {
       this.modalController.dismiss(
